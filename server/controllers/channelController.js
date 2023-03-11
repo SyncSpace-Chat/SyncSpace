842a4b57aa3bbc9a5196d24a40e415a99220deab
gilessteiner1 <gilessteiner1@gmail.com>
2023-03-11 10:03:38 -0500

    Added channelController middleware to get add and send messages

--- server/controllers/channelController.js
+++ server/controllers/channelController.js
@@ -2,4 +2,47 @@ const Channel = require('../models/channelModel');
 
 const channelController = {};
 
+// Giles Steiner
+//
+// Input: 
+// req.body.channel contains the channel name 
+//
+// Purpose: Pulls an array of messages corrosponding to a specific channel  
+// and adds them to res.locals.messages
+//
+// Output: null
+channelController.getMessages = async (req, res, next) => {
+    const channel = req.body.channel;
+    const channelCollectionArr = await Channel.find({ channelName: channel })[0];
+    res.locals.messages = channelCollectionArr.messages;
+    return next();
+}
+
+// Giles Steiner
+//
+// Input: 
+// req.body.message contains the message content 
+// req.body.channel contains the channel name 
+// req.cookies.user contains the username of who is sending the message
+//
+// Purpose: Adds a new message to the collection corrosponding to a specific channel 
+//
+// Output: null
+channelController.sendMessage = async (req, res, next) => {
+    const channel = req.body.channel;
+    const message = req.body.message;
+    const username = req.cookies.user;
+    const channelCollectionArr = await Channel.find({ channelName: channel })[0];
+    const messageArr = channelCollectionArr.messages;
+    messageArr.push({
+        message: message,
+        username: username
+    });
+
+    //update channel collection
+    await Channel.findOneAndUpdate({ channelName: channel },
+        { messages: messageArr });
+    return next();
+}
+
 module.exports = channelController;
