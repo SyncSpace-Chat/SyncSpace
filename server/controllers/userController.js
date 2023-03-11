a1c2deae4a7fe3003c193f5ff7863e97be275563
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-03-11 09:54:34 -0500

    Routes Created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,15 @@
 const User = require('../models/userModel');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userController = {};
 
 userController.createUser = (req,res,next) => {
   bcrypt.hash(req.body.password, 10, function(err, hash) {
-    User.create({username: req.body.username, password: hash})
+    User.create({username: req.body.username, password: hash, subscribedChannels: []})
     .then(() => {
       console.log('successfully added user to the database');
-      res.cookie('verified user', req.body.username);
+      res.cookie('user', req.body.username);
       return next();
     })
     .catch((err) => {
@@ -26,7 +26,7 @@ userController.verifyUser = async (req, res, next) => {
   if (comparison) {
     console.log('correct password');
     //store the username as a cookie value
-    res.cookie('verified user', req.body.username);
+    res.cookie('user', req.body.username);
     return next();
   } else {
     console.log('wrong password');
--- server/routes/db_server.js
+++ server/routes/db_server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+const channelController = require('../controllers/channelController');
+const bcrypt = require('bcryptjs');
 
+// Express json preparations
 //test that server is set up correctly GS
 router.get('/*', (req, res) => {
     console.log("in the server now")
@@ -8,10 +12,16 @@ router.get('/*', (req, res) => {
 });
 
 //router.post('/signup')
-//do we want to redirect users to login after they register their account or just log them in automatically?
+router.post('/signup', userController.createUser, (req, res) => {
+    console.log('Signing process started');
+    res.status(200).json('User created');
+});
+//do we want to redirect users to login after they register their account or just log them in automatically?  //- Log them in automatically ofc -M
 //Would be slightly less coding to send them to the login page after account creation
-
-//router.get('/login')
+router.post('/login', userController.verifyUser, (req, res) => {
+    console.log('Verifying User');
+    res.status(200).json('User verified')
+});
 
 
 module.exports = router;
