c2dd1cdab6ecf76e380edc68c78bd27bfd78ad5e
Yomkool <bannon.pj@gmail.com>
2023-03-10 21:41:58 -0500

    "added form submission for login and signup. Still need server routing to make this work"

--- server/routes/db_server.js
+++ server/routes/db_server.js
@@ -7,4 +7,11 @@ router.get('/*', (req, res) => {
     res.status(200).json("You reached server");
 });
 
+//router.post('/signup')
+//do we want to redirect users to login after they register their account or just log them in automatically?
+//Would be slightly less coding to send them to the login page after account creation
+
+//router.get('/login')
+
+
 module.exports = router;
