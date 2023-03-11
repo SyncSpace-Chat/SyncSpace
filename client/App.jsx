bddb4b74427ededc40d44945f1f769b673ae2a6c
Timothy Muller <timothymuller@Timothys-MacBook-Pro-2.local>
2023-03-11 14:13:02 -0500

    built out entire frontend and integrated send and get messages.
    Co-authored-by: giles gilessteiner1@gmail.com

--- client/App.jsx
+++ client/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Switch, Route } from "react-router-dom";
 import Header from './components/Header';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import LBar from './components/LBar.jsx'
 import './stylesheets/styles.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <>
             {/* <Header /> */}
             <Switch>
+                <Route exact path="/window" component={LBar} />
                 <Route exact path="/login" component={Login} />
                 <Route path="/signup" component={Signup} />
             </Switch>
