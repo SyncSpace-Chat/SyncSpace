159426b5d3b3497152c39ca4d3ac902b0d9bf53f
gilessteiner1 <gilessteiner1@gmail.com>
2023-03-13 15:22:24 -0400

    m

--- client/components/LBar.jsx
+++ client/components/LBar.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
 import ChatWindow from './ChatWindow.jsx';
 import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import { motion } from 'framer-motion';
 
 export default function LBar() {
-
     // Tim Muller
     //
     // State thing is added so that the channel can be updated
@@ -35,18 +34,20 @@ export default function LBar() {
 
     // Giles Steiner
     //
-    // Purpose: pulls the list of channels that exist in the database 
+    // Purpose: pulls the list of channels that exist in the database
     // and only show the ones that match with the users cookie preference
     useEffect(() => {
         const intervalId = setInterval(() => {
             async function getChannels() {
                 await fetch('./db/getChannels', {
-                    headers: { 'Content-Type': 'application/json' }
+                    headers: { 'Content-Type': 'application/json' },
                 })
                     .then((response) => response.json())
                     .then((data) => {
                         setChannels(data);
-                        const userChannels = data.filter((el) => Cookies.get('subscribedChannels').includes(el));
+                        const userChannels = data.filter((el) =>
+                            Cookies.get('subscribedChannels').includes(el)
+                        );
                         setUserChannels(userChannels);
                     })
                     .catch((error) => {
@@ -63,7 +64,7 @@ export default function LBar() {
     // Whenever a string or anything is typed into the add channel box the channel name to be added is updated immediately
     const handleChannelName = (e) => {
         setChannel(e.target.value);
-    }
+    };
 
     // Tim Muller
     //
@@ -83,8 +84,8 @@ export default function LBar() {
     function logOut() {
         Cookies.remove('user');
         Cookies.remove('ownedChannels');
-        Cookies.remove('subscribedChannels')
-        window.location.href = "/login";
+        Cookies.remove('subscribedChannels');
+        window.location.href = '/login';
     }
 
     // Giles Steiner
@@ -96,16 +97,18 @@ export default function LBar() {
     }
 
     // Giles Steiner
-    // 
-    // When user clicks a new message from the drop down menu a PUT request is done to 
+    //
+    // When user clicks a new message from the drop down menu a PUT request is done to
     // db/subscribe subscribing the user to that channel and updating the cookie preference
     async function browseChannelClick() {
         // console.log("clicked: ", document.getElementById('browseChannelName').value);
-        console.log("subscribing to new channel");
+        console.log('subscribing to new channel');
         await fetch('./db/subscribe', {
             method: 'PUT',
-            body: JSON.stringify({ channel: document.getElementById('browseChannelName').value }),
-            headers: { 'Content-Type': 'application/json' }
+            body: JSON.stringify({
+                channel: document.getElementById('browseChannelName').value,
+            }),
+            headers: { 'Content-Type': 'application/json' },
         })
             .then((response) => response.json())
             .catch((error) => {
@@ -115,31 +118,35 @@ export default function LBar() {
 
     // Giles Steiner
     //
-    // channels.map is iterating through entire channels state and if an element is not already 
-    // in the users cookie preferences it is displayed on a dropdown menu as a new channel for the 
-    // user to subscrbie to 
+    // channels.map is iterating through entire channels state and if an element is not already
+    // in the users cookie preferences it is displayed on a dropdown menu as a new channel for the
+    // user to subscrbie to
 
     // userChannels.map is iterating through userChannels state and rendering a button for each channel
-    // in the users cookie prefences 
+    // in the users cookie prefences
     // onclick invokes click handler function which changes currentChannel state
     //
     // Chat window is rendered as a child component and the current channel is passed down
     // as a prop
     return (
         <>
-            <div className="chatPage">
-                <form id="browseChannels">
+            <div className='chatPage'>
+                <form id='browseChannels'>
                     <label>Add a new channel: </label>
-                    <select id="browseChannelName">
-                        //add motion thing here for channel
+                    <select id='browseChannelName'>
+            //add motion thing here for channel
                         {channels.map((channel) => {
                             if (!Cookies.get('subscribedChannels').includes(channel)) {
-                                return <option value={channel}>{channel}</option>
+                                return <option value={channel}>{channel}</option>;
                             }
                         })}
                     </select>
                     <br></br>
-                    <button type='button' className='sendButton' onClick={browseChannelClick}>
+                    <button
+                        type='button'
+                        className='sendButton'
+                        onClick={browseChannelClick}
+                    >
                         Subcribe
                     </button>
                 </form>
