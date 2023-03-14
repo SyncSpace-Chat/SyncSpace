648c281a008d24441be4f767c789747272e4a710
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-03-14 15:46:28 -0400

    junaid- created new database and connected to it

--- server/server.js
+++ server/server.js
@@ -1,51 +1,65 @@
-const path = require('path');
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const dbRouter = require('./routes/db_server');
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
+const dbRouter = require("./routes/db_server");
+const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = 3000;
-// .ENV key variable name goes here -> each user must create their own ENV key, which all need to be the same URI key.
-const URIkey = process.env.SHARK_DB_KEY;
+
+//JUNAID AHMED
+//
+//
+//ALL USERS CREATE A .ENV FILE IN THE ROOT DIRECTORY OF THE PROJECT
+//IN THE .ENV FILE CREATE THIS: MONGO_URI="OUR URI KEY(IN THE SLACK)"
+const URIkey = process.env.MONGO_URI;
 const MONGO_URI = URIkey;
 
-mongoose.connect(MONGO_URI, {
+mongoose
+  .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'shark-db'
-})
-    .then(() => console.log('Connected to Mongo DB.'))
-    .catch(err => console.log(err));
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  })
+  .catch(() => {
+    console.log("DB FAIL");
+  });
 
 //handle request parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/db/static', express.static(path.resolve(__dirname, '../client/sounds/')));
+app.use(
+  "/db/static",
+  express.static(path.resolve(__dirname, "../client/sounds/"))
+);
 
-app.use('/db', dbRouter); //route all 3000 requests to db file
-
+app.use("/db", dbRouter); //route all 3000 requests to db file
 
 //catch-all route handler for any requests to an unknown route
-app.use((req, res) => res.status(404).send('The page you are looking for does not exist'));
+app.use((req, res) =>
+  res.status(404).send("The page you are looking for does not exist")
+);
 
-//global error handler 
+//global error handler
 app.use((err, req, res, next) => {
-    const defaultErr = {
-        log: 'Express error handler caught unknown middleware error',
-        status: 500,
-        message: { err: 'An error occurred' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    console.log(errorObj.log);
-    return res.status(errorObj.status).json(errorObj.message);
+  const defaultErr = {
+    log: "Express error handler caught unknown middleware error",
+    status: 500,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 //start server
 app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}...`);
+  console.log(`Server listening on port: ${PORT}...`);
 });
 
 module.exports = app;
