81f8da3cb3924e1b287322f9432af8f4ad688498
Junaid Ahmed <junaid7ahmed96@gmail.com>
2023-03-16 12:00:10 -0400

    minor changes

--- client/components/ChatWindow.jsx
+++ client/components/ChatWindow.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
+import React, { useState, useEffect, useRef, useContext } from "react";
 import ChatBubble from "./ChatBubble.jsx";
 import Cookies from "js-cookie";
-
-import { currentChannelStore } from "../store.js";
-import { WsContext } from '../webSocketProvider.jsx';
+import { WsContext } from "../webSocketProvider.jsx";
+import { channelStore } from "../store";
 
 export default function ChatWindow(props) {
-  const { currentChannel } = currentChannelStore();
+  const { currentChannel } = channelStore();
+  console.log(currentChannel);
   // const wsRef = new WebSocket('ws://localhost:8082')
 
   //Giles Steiner
@@ -23,38 +23,40 @@ export default function ChatWindow(props) {
   // const [ws, setWs] = useState(new WebSocket("ws://localhost:8082"))
   // useEffect(() => {
   webSocket.onmessage = ({ data }) => {
-    const parsed = JSON.parse(data)
+    const parsed = JSON.parse(data);
     if (parsed.messages) {
-      const newMessage = parsed.messages[parsed.messages.length - 1]
-      console.log(parsed.messages[parsed.messages.length - 1])
+      const newMessage = parsed.messages[parsed.messages.length - 1];
+      console.log(parsed.messages[parsed.messages.length - 1]);
       const newChats = [...chats];
-      newChats.push({ message: newMessage.message, username: newMessage.username, _id: newMessage._id})
-      setChats(newChats)
+      newChats.push({
+        message: newMessage.message,
+        username: newMessage.username,
+        _id: newMessage._id,
+      });
+      setChats(newChats);
     }
-    console.log('server sent this')
-  }
+    console.log("server sent this");
+  };
   // }, [chats])
-
 
   useEffect(() => {
     // if (!ws && webSocket) {
     //   setWs(webSocket);
     // }
-    console.log('mount', testRef.current, webSocket)
-    setWs(webSocket)
+    console.log("mount", testRef.current, webSocket);
+    setWs(webSocket);
 
     if (webSocket) {
       // setWs(ws)
-      webSocket.onopen = function(e) {
-        console.log('connection established')
-      }
+      webSocket.onopen = function (e) {
+        console.log("connection established");
+      };
       //   console.log('clicked2')
       //   ws.addEventListener("open", (e) => {
       //     ws.send('message'.toString())
       //   })
     }
-
-  }, [])
+  }, []);
 
   //Giles Steiner
   //
@@ -66,18 +68,21 @@ export default function ChatWindow(props) {
   //Giles Steiner
   //
   //When the user send a message send a post request to /db/sendMessage route
-  //route is caught in db_server 
+  //route is caught in db_server
   const handleSubmit = async (e) => {
     // await fetch('./db/sendMessage', {
     //   method: 'POST',
     //   body: JSON.stringify({ message: message, channel: currentChannel }),
     //   headers: { 'Content-Type': 'application/json' },
     // });
-    const user = document.cookie.split('; ').find(e => e.startsWith("user="))?.split("=")[1]
-    webSocket.send(JSON.stringify({ message, currentChannel, user }))
-    setMessage('');
-    let stuff = document.getElementById('inputMessage');
-    stuff.value = '';
+    const user = document.cookie
+      .split("; ")
+      .find((e) => e.startsWith("user="))
+      ?.split("=")[1];
+    webSocket.send(JSON.stringify({ message, currentChannel, user }));
+    setMessage("");
+    let stuff = document.getElementById("inputMessage");
+    stuff.value = "";
     const audio = new Audio("db/static/iphone_woosh.mp3");
     audio.play();
   };
@@ -95,13 +100,12 @@ export default function ChatWindow(props) {
   // Every 500 seconds a fetch request is done to db/getMessages to get all the current messages in
   // the channel the user is currently in. The chats state is concurrently updated
   useEffect(() => {
-    // const intervalId = setInterval(() => {
     async function getMessages() {
       if (currentChannel != "") {
-        await fetch('./db/getMessages', {
-          method: 'POST',
+        await fetch("./db/getMessages", {
+          method: "POST",
           body: JSON.stringify({ channel: currentChannel }),
-          headers: { 'Content-Type': 'application/json' }
+          headers: { "Content-Type": "application/json" },
         })
           .then((response) => response.json())
           .then((data) => {
@@ -109,25 +113,19 @@ export default function ChatWindow(props) {
           })
 
           .catch((error) => {
-            console.error('Error in grabbing chats from channel:', error);
+            console.error("Error in grabbing chats from channel:", error);
           });
-
       }
     }
     getMessages();
-    // }, 500);
-
-    return () => clearInterval(intervalId);
   }, [currentChannel]);
-
 
   useEffect(() => {
     if (messageBoxRef.current) {
       messageBoxRef.current.scrollIntoView({ behavior: "smooth" });
     }
-    console.log("chat's changed")
+    console.log("chat's changed");
   }, [chats]);
-
 
   const messageCache = {};
   const chatBubbles = [];
@@ -178,12 +176,10 @@ export default function ChatWindow(props) {
           <input type="text" id="inputMessage" onChange={handleMessage} />
         </div>
         <div id="messageSubmit">
-
           <button type="button" className="sendButton" onClick={handleSubmit}>
             Send
           </button>
         </div>
-
       </div>
     </div>
   );
